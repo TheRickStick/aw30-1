c7a13d1fa1580eb5339a1640ccd939e145bb154b
TheRickStick <114582372+TheRickStick@users.noreply.github.com>
2023-03-28 12:21:40 +0200

    Added new feature to the code

---
+++ routes/multiply.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var jsend = require('jsend');
+var router = express.Router();
+
+router.use(jsend.middleware);
+router.get('/:number1/:number2', function(req, res, next) {
+    const number1 = parseInt(req.params.number1);
+    if(isNaN(number1)) {
+        return res.jsend.fail({"number1": "number1 is not in correct format"});
+    }
+    const number2 = parseInt(req.params.number2);
+    if(isNaN(number2)) {
+        return res.jsend.fail({"number2": "number2 is not in correct format"});
+    }
+    const result = number1 * number2;
+    res.jsend.success({"result": result});
+});
+
+module.exports = router;
